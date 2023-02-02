38d4259b4389bc620055a034bc7c73ea7683f4c7
Neha Sharan <sunshinesharan12@gmail.com>
2023-02-02 11:50:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './App.css'
 
-function App() {
+const Quote = () => {
+  const [quote, setQuote] = useState({});
+  const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
+  
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+  
+  const fetchQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(data => {
+        setQuote(data);
+        setBackgroundColor(getRandomColor());
+      });
+  };
+  
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote" style={{ backgroundColor, height: "100vh", padding: "2em" }}>
+      <h2>{quote.content}</h2>
+      <p>— {quote.author}</p>
+      <button className="btn" onClick={fetchQuote}>New Quote</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Quote;
+
+
+
